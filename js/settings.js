0a2addb05503504487438c79046b46a2ef662d3a
Quentin Decaunes <le.storm1er@gmail.com>
2019-02-27 20:16:11 +0100

    Auto reindent

--- js/settings.js
+++ js/settings.js
@@ -1,22 +1,22 @@
 function ready(fn) {
-    if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
-      fn();
-    } else {
-      document.addEventListener('DOMContentLoaded', fn);
+  if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
+    fn();
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
+}
+ready(function(){
+  var ranges = document.querySelectorAll('[repeat]');
+  for (const range in ranges) {
+    if (ranges.hasOwnProperty(range)) {
+      const element = ranges[range];
+      element.addEventListener('change', function(event) {
+        var repeater = document.getElementById(event.target.attributes.repeat.value);
+        repeater.value = event.target.value;
+      });
     }
   }
-  ready(function(){
-    var ranges = document.querySelectorAll('[repeat]');
-    for (const range in ranges) {
-      if (ranges.hasOwnProperty(range)) {
-        const element = ranges[range];
-        element.addEventListener('change', function(event) {
-          var repeater = document.getElementById(event.target.attributes.repeat.value);
-          repeater.value = event.target.value;
-        });
-      }
-    }
-  });
+});
 /*
 stapm_limit_w_range
 ppt_fast_limit_w_range
