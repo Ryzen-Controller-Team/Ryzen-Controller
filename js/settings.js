9492952e162f019c153928d31d1d4e0ad74c7dc7
Quentin Decaunes <le.storm1er@gmail.com>
2019-02-27 14:00:19 +0100

    Added uikit and basic visual stuff.

---
+++ js/settings.js
@@ -0,0 +1,26 @@
+function ready(fn) {
+    if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
+      fn();
+    } else {
+      document.addEventListener('DOMContentLoaded', fn);
+    }
+  }
+  ready(function(){
+    var ranges = document.querySelectorAll('[repeat]');
+    for (const range in ranges) {
+      if (ranges.hasOwnProperty(range)) {
+        const element = ranges[range];
+        element.addEventListener('change', function(event) {
+          var repeater = document.getElementById(event.target.attributes.repeat.value);
+          repeater.value = event.target.value;
+        });
+      }
+    }
+  });
+/*
+stapm_limit_w_range
+ppt_fast_limit_w_range
+ppt_slow_limit_w_range
+temperature_limit_c_range
+vrm_current_m_a_range
+*/
