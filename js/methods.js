7182f1d9a55a20111d54dd9fa5da5bd70e2477a5
Quentin Decaunes <le.storm1er@gmail.com>
2019-03-02 16:21:31 +0100

    Removed useless logs.

--- js/methods.js
+++ js/methods.js
@@ -263,11 +263,9 @@ function reApplyPeriodically(seconds) {
  * Display tooltip on each options.
  */
 function displayOptionDescription() {
-  appendLog("displayOptionDescription():");
   const options_description = require('./js/options_description.json');
   for (const option in options_description) {
     if (options_description.hasOwnProperty(option)) {
-      appendLog(`- option: ${option}`);
       const description = options_description[option];
       const node = document.getElementById(option).parentElement.parentElement;
       node.setAttribute('uk-tooltip', description);
