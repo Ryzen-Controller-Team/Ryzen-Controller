52ad83863df1383fa4cffc85df23a3be43912417
Quentin Decaunes <le.storm1er@gmail.com>
2019-06-05 13:50:46 +0200

    Updated ryzenadj to 0.5.1.

--- bin/demo.bat
+++ bin/demo.bat
@@ -1,2 +1,2 @@
-%~dp0\ryzenadj.exe --stapm-limit=4000 --fast-limit=5000 --slow-limit=5000 --tctl-temp=90
+%~dp0\ryzenadj.exe --stapm-limit=40000 --fast-limit=45000 --slow-limit=45000 --tctl-temp=90
 pause
