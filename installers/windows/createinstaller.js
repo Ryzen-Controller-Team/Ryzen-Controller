068aeff3841698b70120855e3b004555fd2cf388
Quentin DECAUNES <le.storm1er@gmail.com>
2019-07-05 12:53:00 +0200

    Updated win32-installer jobs.

--- installers/windows/createinstaller.js
+++ installers/windows/createinstaller.js
@@ -1,15 +1,24 @@
 const createWindowsInstaller = require('electron-winstaller').createWindowsInstaller
 const path = require('path')
 
+var keepAlive = false;
+
 getInstallerConfig()
 .then(createWindowsInstaller)
 .catch((error) => {
   console.error(error.message || error)
   process.exit(1)
 })
+.then(function(){
+  clearInterval(keepAlive);
+  console.log('Installer created.');
+})
 
 function getInstallerConfig () {
   console.log('creating windows installer')
+  keepAlive = setInterval(function(){
+    console.log(`I'm alive ...`);
+  }, 10000);
   const rootPath = path.join('./')
   
   return Promise.resolve({
