dbf670287377d4948ee1fe3dc2f89e42239cea5f
Quentin DECAUNES <le.storm1er@gmail.com>
2019-08-06 14:20:20 +0200

    fix(developers): #40 Removing sentry noise on preset import caused by malformed data.

--- js/preset.js
+++ js/preset.js
@@ -25,7 +25,6 @@ function preset_import() {
   } catch (e) {
     notification('danger', 'Unable to import presets, malformed data.');
     appendLog(`preset_import() ${e}`);
-    Sentry.captureException(`preset_import() ${e}`);
     return;
   }
 
